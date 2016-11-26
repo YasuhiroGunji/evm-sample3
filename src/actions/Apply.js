99ff1ab04ba150ec9bf95bd5e228379349eaa7c9
YasuhiroGunji <YasuhiroGunji19861025@gmail.com>
2016-11-26 10:54:19 +0900

    commit

--- src/actions/Apply.js
+++ src/actions/Apply.js
@@ -1,3 +1,12 @@
 export const ACTION_SUBMIT = 'SUBMIT';
 
-export const submit = ()
+export const submit = () => {
+  return (
+    type: ACTION_SUBMIT,
+    date: PropTypes.object.isRequired,
+  kokyakuCd: PropTypes.string.isRequired,
+  projectCd: PropTypes.string.isRequired,
+  startTime: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  )
+};
--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -1,28 +1,11 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
-import Paper from 'material-ui/Paper';
-import TextField from 'material-ui/TextField';
-import FontIcon from 'material-ui/FontIcon';
-import RaisedButton from 'material-ui/RaisedButton';
-
-import {grey400, yellow600, darkBlack, lightBlack} from 'material-ui/styles/colors';
-import {List, ListItem} from 'material-ui/List';
-
-import DatePicker from 'material-ui/DatePicker';
-import Avatar from 'material-ui/Avatar';
-import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
-import FileFolder from 'material-ui/svg-icons/file/folder';
-
-import { Link } from 'react-router';
-
-class App extends Component {
+export default class App extends Component {
 
   constructor(props) {
     super(props);
