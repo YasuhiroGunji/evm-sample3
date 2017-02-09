ebc0a25d1b98d5a28173e9970d5a318120e9025f
YasuhiroGunji <YasuhiroGunji19861025@gmail.com>
2017-02-10 08:35:16 +0900

    Update Api.js

--- src/actions/Api.js
+++ src/actions/Api.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 export default (apiName) => {
   return new Promise(
     (resolve, reject) => {
-      request.get("http://evmapp20170130101609.azurewebsites.net/api/" + apiName)
+      request.get("http://xxxxxxxxxx.azurewebsites.net/api/" + apiName)
         .end(
           (err, res) => {
             if (err) {
