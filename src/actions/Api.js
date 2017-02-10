aa64440efcd79d7e7d1132ed07416a1ab4d69406
YasuhiroGunji <YasuhiroGunji19861025@gmail.com>
2017-02-10 20:41:32 +0900

    Api Post 追加

--- src/actions/Api.js
+++ src/actions/Api.js
@@ -1,6 +1,22 @@
 import request from 'superagent';
 
-export default (apiName) => {
+export const Get = (apiName) => {
+  return new Promise(
+    (resolve, reject) => {
+      request.get("http://xxxxxxxxxx.azurewebsites.net/api/" + apiName)
+        .end(
+          (err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(res.text));
+            }
+          }
+        );
+    }
+  );
+}
+export const Post = (apiName) => {
   return new Promise(
     (resolve, reject) => {
       request.get("http://xxxxxxxxxx.azurewebsites.net/api/" + apiName)
--- src/actions/Apply.js
+++ src/actions/Apply.js
@@ -24,7 +24,7 @@ export const init = (enpId) => {
 
 export const submit = (applyForm) => {
 
-  // API.callApi();
+  API("Apply/GetOvertimeList/42015")
 
   return (dispatch) => {
     dispatch({
