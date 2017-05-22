f99c6b76d8d80bf3eba56664cae5fa734f5ba4c7
YasuhiroGunji <YasuhiroGunji19861025@gmail.com>
2017-05-22 19:34:16 +0900

    commit

--- src/actions/Api.js
+++ src/actions/Api.js
@@ -3,6 +3,27 @@ import request from 'superagent';
 // const url = 'http://evmapp20170130101609.azurewebsites.net/api/';
 const url = 'http://localhost:18593/api/v1';
 
+
+export const GetUri = (api, action, arrayParams) => {
+  const strParams = arrayParams.join('/');
+  return new Promise(
+    (resolve, reject) => {
+      request
+        .get(`${url}/${api}/${action}/${strParams}`) // api/action/param1/param2/...
+        .end(
+          (err, res) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(JSON.parse(res.text));
+            }
+          },
+        );
+    },
+  );
+};
+
+
 export const Get = (apiName, param1, param2) => {
   return new Promise(
     (resolve, reject) => {
